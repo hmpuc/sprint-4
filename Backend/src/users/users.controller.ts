ab8615e8c4427dfb04e8e668460cbdf45cb7399d
hmpuc <henrique.mairesse@edu.pucrs.br>
2025-04-09 16:35:34 -0300

    Commit

--- Backend/src/users/users.controller.ts
+++ Backend/src/users/users.controller.ts
@@ -1,13 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, ParseIntPipe, Patch, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Prisma } from '@prisma/client';
+
+interface UpdateLevelDto {
+  level: number;
+}
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() user: Prisma.UserCreateInput) {
+  async create(@Body() user: Prisma.UserCreateInput) {
     return this.usersService.create(user);
+  }
+
+  @Get()
+  async findAll() {
+    return this.usersService.findAll()
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':id') 
+  async update(@Param('id', ParseIntPipe) id: number, @Body() user: Prisma.UserUpdateInput) {
+    return this.usersService.update(id, user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
+  }
+
+  @Patch(':id/level') 
+  async updateLevel(@Param('id', ParseIntPipe) id: number, @Body() updateLevelDto: UpdateLevelDto) {
+    console.log(updateLevelDto)
+    return this.usersService.updateLevel(id, updateLevelDto.level);
   }
 }
--- Backend/src/users/users.service.ts
+++ Backend/src/users/users.service.ts
@@ -5,7 +5,48 @@ import { PrismaService } from 'src/prisma.service';
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+  
   create(user: Prisma.UserCreateInput) {
     return this.prisma.user.create({ data: user });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.user.findUnique({ 
+      where: {
+        id: id
+      }
+    })
+  }
+
+  update(id: number, user: Prisma.UserUpdateInput) {
+    return this.prisma.user.update({ 
+      where: {
+        id: id
+      },
+      data: user
+    })
+  }
+
+  delete(id: number) {
+    return this.prisma.user.delete({
+      where: {
+        id: id
+      }
+    })
+  }
+
+  updateLevel(id: number, level: number) {
+    return this.prisma.user.update({ 
+      where: {
+        id: id
+      },
+      data: {
+        level: level
+      }
+    })
   }
 }
