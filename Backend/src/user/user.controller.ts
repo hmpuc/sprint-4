78787aec55beba25a2e549e534b1eeface5e2868
hmpuc <henrique.mairesse@edu.pucrs.br>
2025-04-09 15:14:06 -0300

    oi

--- Backend/src/user/user.controller.ts
+++ Backend/src/user/user.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
-    
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() user: Prisma.UserCreateInput) {
+    return this.userService.create(user);
+  }
 }
--- Backend/src/user/user.service.ts
+++ Backend/src/user/user.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
-
+  constructor(private prisma: PrismaService) {}
+  create(user: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data: user });
+  }
 }
