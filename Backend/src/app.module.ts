42e9dc88ee10c36130d2fd68312f2017d7139185
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-04-11 16:08:36 -0300

    room.dto e http status

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [UsersModule, RoomsModule, AuthModule],
+  imports: [UsersModule, RoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
