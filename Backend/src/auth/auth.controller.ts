8e7c3c41844a56679748d30bc3ee216f81bcecce
hmpuc <henrique.mairesse@edu.pucrs.br>
2025-04-14 16:26:24 -0300

    Commit

--- Backend/src/auth/auth.controller.ts
+++ Backend/src/auth/auth.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from 'src/public.decorator';
 
-@Controller('login')
+@Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    @Post()
+    @Post('login')
     @Public()
     signIn(@Body() signInDto: Record<string, any>) {
       return this.authService.signIn(signInDto.email, signInDto.password);
