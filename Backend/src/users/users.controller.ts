711cdcc10c9ad9345167493070af1291080edbd6
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-04-16 14:07:38 -0300

    roles 4

--- Backend/src/users/users.controller.ts
+++ Backend/src/users/users.controller.ts
@@ -1,50 +1,49 @@
-import { Body, Controller, Post, Get, Param, ParseIntPipe, Patch, Delete, UsePipes, ValidationPipe, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, ParseIntPipe, Patch, Delete, UsePipes, ValidationPipe, UseGuards, SetMetadata } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUser, UpdateUser, VerifyLevel } from './users.dto'
-//import { Roles } from '../roles/roles.decorator';
-//import { RolesGuard } from '../roles/roles.guard';
-//import { Role } from '../roles/roles.enum';
+import { Roles } from '../roles/roles.decorator';
+import { RolesGuard } from '../roles/roles.guard';
+import { Role } from '../roles/roles.enum';
+import { Public } from 'src/public.decorator';
 
-//@UseGuards(RolesGuard)
+@UseGuards(RolesGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   
   
   @Post()
+  @Roles(Role.Admin)
   @UsePipes(new ValidationPipe())
   async create(@Body() user: CreateUser) {
     return this.usersService.create(user);
   }
   
-
   @Get()
-  //@Roles(Role.Admin, Role.CommonUser, Role.IntermediateUser2, Role.IntermediateUser3, Role.IntermediateUser4)
   async findAll() {
     return this.usersService.findAll()
   }
   
   @Get(':id')
-  //@Roles(Role.Admin, Role.CommonUser, Role.IntermediateUser2, Role.IntermediateUser3, Role.IntermediateUser4)
   async findOne(@Param('id', ParseIntPipe) id: number) {
     console.log(id)
     return this.usersService.findOne(id);
   }
 
   @Patch(':id') 
-  //@Roles(Role.Admin)
+  @Roles(Role.Admin)
   async update(@Param('id', ParseIntPipe) id: number, @Body() user: UpdateUser) {
     return this.usersService.update(id, user);
   }
 
   @Delete(':id')
-  //@Roles(Role.Admin)
+  @Roles(Role.Admin, Role.IntermediateUser4)
   async delete(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.delete(id);
   }
 
   @Patch(':id/level') 
-  //@Roles(Role.Admin)
+  @Roles(Role.Admin)
   async updateLevel(@Param('id', ParseIntPipe) id: number, @Body() updateLevelDto: VerifyLevel) {
     return this.usersService.updateLevel(id, updateLevelDto.level);
   }
