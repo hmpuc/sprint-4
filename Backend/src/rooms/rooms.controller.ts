de400dd1532ecb965a13c8844c18d6417ea0b9f9
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-04-22 17:04:14 -0300

    faltou roles nas salas na main

--- Backend/src/rooms/rooms.controller.ts
+++ Backend/src/rooms/rooms.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Post, Patch, Delete, Param, Body, ParseIntPipe, UsePipes, ValidationPipe, Put} from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, ParseIntPipe, UsePipes, ValidationPipe, Put, UseGuards} from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { CreateRoom, UpdateRoom} from './rooms.dto'
+import { Role } from 'src/roles/roles.enum';
+import { Roles } from 'src/roles/roles.decorator';
 
+@UseGuards()
 @Controller('places')
 export class RoomsController {
     constructor(private readonly roomsService: RoomsService) {}
+
+    @Roles(Role.Admin)
     @Post('')
     async create(@Body() room: CreateRoom) {
         return this.roomsService.create(room);
@@ -20,11 +25,13 @@ export class RoomsController {
         return this.roomsService.findOne(id);
     }
 
+    @Roles(Role.Admin, Role.IntermediateUser4)
     @Put(':id') 
     async update(@Param('id', ParseIntPipe) id: number, @Body() room: UpdateRoom) {
         return this.roomsService.update(id, room);
     }
 
+    @Roles(Role.Admin)
     @Delete(':id')
     async delete(@Param('id', ParseIntPipe) id: number) {
         return this.roomsService.delete(id);
